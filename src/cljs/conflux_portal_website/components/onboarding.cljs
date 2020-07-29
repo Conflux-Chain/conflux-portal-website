e51b14ff56f4f526ac1372c3b033d07e6fb43d94
yqrashawn <namy.19@gmail.com>
2020-07-29 10:24:42 +0800

    feat: portal v0.3.3

--- src/cljs/conflux_portal_website/components/onboarding.cljs
+++ src/cljs/conflux_portal_website/components/onboarding.cljs
@@ -5,7 +5,7 @@
 
 ;; debug onboarding package
 (set! (.-FAKE_PORTAL_SITE js/window) true)
-(def onboarding (Onboarding. (js-obj "cachedPortalVersion" "0.3.2" "ignoreSessionStorage" true)))
+(def onboarding (Onboarding. (js-obj "cachedPortalVersion" "0.3.3" "ignoreSessionStorage" true)))
 
 (defn portal-installed? []
   (Onboarding/isConfluxPortalInstalled))
